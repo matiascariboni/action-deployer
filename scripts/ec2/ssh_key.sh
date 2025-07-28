9ea1c8ae8b23f93c4965f2c779d1938d1661b588
Matías Cariboni <mcariboni@icloud.com>
2025-07-28 18:43:13 +0200

    First test

--- scripts/ec2/ssh_key.sh
+++
@@ -1,4 +0,0 @@
-echo "${{ secrets.EC2_KEY }}" | tr -d '\r' > server_key.pem
-          chmod 600 server_key.pem
-          mkdir -p ~/.ssh
-          ssh-keyscan -H ${{ secrets.EC2_IP }} >> ~/.ssh/known_hosts
